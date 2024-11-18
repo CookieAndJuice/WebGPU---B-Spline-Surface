a07d9f21dc403e9702765aea693cc2642c72e6c3
CHLee <cradiacradia@gmail.com>
2024-11-18 17:44:52 +0900

    [저장용] VS FS 구현 중

--- WGSL VS_FS.js
+++ WGSL VS_FS.js
@@ -2,9 +2,23 @@
 export function vertexShaderSrc()
 {
     return /*wgsl*/`
-        @vertex fn vs()
+        struct Vertex {
+            @location(0) position: vec2f,
+            @location(1) size: f32,
+        };
+
+        struct VSOutput {
+            @builtin(position) position: vec4f,
+        };
+
+        @vertex fn vs() -> VSOutput
         {
 
+
+            var vsOut: VSOutput;
+
+
+            return vsOut;
         }
     `;
 }
@@ -12,9 +26,13 @@ export function vertexShaderSrc()
 export function fragmentShaderSrc()
 {
     return /*wgsl*/`
-        @fragment fn fs()
+        struct FSInput {
+            @builtin(position) position: vec4f,
+        }
+
+        @fragment fn fs(fsIn: FSInput) -> FSInput
         {
-            
+            return vec4f(0.5, 0.5, 1, 1);
         }
     `;
 }
